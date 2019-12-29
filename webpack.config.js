8c40f01eb5fe6fdf136d58b1effac1154d9ba1d1
Chun Yan Sit <the-gates-of-Zion@outlook.com>
2019-12-29 14:34:45 +0800

    completed output management

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: {
     keyNameA: './src/index.js',
     keyNameB: './src/print.js',
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+
+      title: 'Output Management',
+
+    }),
+
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
