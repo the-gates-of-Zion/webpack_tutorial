4f4a1025c98f8942c165d70a9376a1af756aa489
Chun Yan Sit <the-gates-of-Zion@outlook.com>
2019-12-29 16:07:57 +0800

    complete development

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
+  mode: 'development',
   entry: {
     keyNameA: './src/index.js',
     keyNameB: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
   },
   plugins: [
     new CleanWebpackPlugin(),
